7a755c8c0207f2e08046af4dd7e30ecec3315ac6
bismarkboateng <bismarkb609@gmail.com>
2022-12-22 22:32:13 +0000

    task 2 and eslint errors

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,9 +1,7 @@
 import ClassRoom from "./0-classroom";
 
-function initializeRooms() {
-    return [
-        ClassRoom(19), ClassRoom(20), ClassRoom(34)
-    ]
+function initializeRooms(){
+  return [
+    ClassRoom(19), ClassRoom(20), ClassRoom(34)
+  ]
 }
-
-initializeRooms()
